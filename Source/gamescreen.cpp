6a8c6d96818360925ba8a368b254986197771940
shelterin <94851109+shelterin2006@users.noreply.github.com>
2025-06-24 21:22:27 +0700

    Button press

--- Source/gamescreen.cpp
+++ Source/gamescreen.cpp
@@ -6,5 +6,12 @@ float right(float sizeIndex, float sizeWindow) {
     return sizeWindow - 20.0f - sizeIndex;
 }
 void screenTitle() {
-
+    Rectangle button = {100, 100, 100, 100};
+    DrawRectangleRec(button, BLUE);
+    if (CheckCollisionPointRec(GetMousePosition(), button)) {
+        DrawText("1", 100, 100, 38, RED);
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+            DrawText("2", 150, 150, 38, RED);
+        }
+    }
 }
--- Source/main.cpp
+++ Source/main.cpp
@@ -24,30 +24,31 @@ int main()
     {
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawTextureEx(c, a.point, 0.0f, scale ,WHITE);
-            DrawTextureEx(c, b.point, 0.0f, scale ,WHITE);
-            DrawTextureV(card, {100, 100} ,WHITE);
-            DrawTextureV(card, {200, 200} ,WHITE);
-            if (i == PLAYER_TURN) {
-                DrawText("tr", 55, 55, 20, BLACK);
-            }
-            else if (i == ENEMY_TURN) {
-                DrawText(std::to_string(a.hp).c_str(), 455, 55, 20, BLACK);
-            }
-            f++;
-            if (f % 30 == 0) {
-                NextTurn(&i);
-                if (i == PLAYER_TURN) {
-                    b.hp--;
-                }
-                else if (i == ENEMY_TURN) {
-                    a.hp--;
-                }
-            }
-        DrawRectangle(10, 10, 100, 10, LIGHTGRAY);
-        DrawRectangle(10, 10, (a.hp*100/10), 10, RED);
-        DrawRectangle(600, 10, 100, 10, LIGHTGRAY);
-        DrawRectangle(600, 10, (b.hp*100/8), 10, RED);
+            screenTitle();
+            // DrawTextureEx(c, a.point, 0.0f, scale ,WHITE);
+            // DrawTextureEx(c, b.point, 0.0f, scale ,WHITE);
+            // DrawTextureV(card, {100, 100} ,WHITE);
+            // DrawTextureV(card, {200, 200} ,WHITE);
+            // if (i == PLAYER_TURN) {
+            //     DrawText("tr", 55, 55, 20, BLACK);
+            // }
+            // else if (i == ENEMY_TURN) {
+            //     DrawText(std::to_string(a.hp).c_str(), 455, 55, 20, BLACK);
+            // }
+            // f++;
+            // if (f % 30 == 0) {
+            //     NextTurn(&i);
+            //     if (i == PLAYER_TURN) {
+            //         b.hp--;
+            //     }
+            //     else if (i == ENEMY_TURN) {
+            //         a.hp--;
+            //     }
+            // }
+            // DrawRectangle(10, 10, 100, 10, LIGHTGRAY);
+            // DrawRectangle(10, 10, (a.hp*100/10), 10, RED);
+            // DrawRectangle(600, 10, 100, 10, LIGHTGRAY);
+            // DrawRectangle(600, 10, (b.hp*100/8), 10, RED);
         EndDrawing();
     }
     CloseWindow();
